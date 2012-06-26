50bd484ef2519d6a1a14c376553b5b4547ad4797
Hao Yao <galaxy.yao@gmail.com>
2012-06-26 15:21:00 +0800

    rename behavior alicedollsummon

---
+++ trunk/TouhouSpring/Cards/Behaviors/Passive_AliceDollSummon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TouhouSpring.Triggers;
+
+namespace TouhouSpring.Behaviors
+{
+    public class Passive_AliceDollSummon : BaseBehavior<Passive_AliceDollSummon.ModelType>,
+        ITrigger<PlayerTurnStartedContext>
+    {
+        public void Trigger(PlayerTurnStartedContext context)
+        {
+            if (context.Game.InPlayerPhases && IsOnBattlefield && context.Game.PlayerPlayer == Host.Owner)
+            {
+                1.Repeat(i =>
+                {
+                    var card = new BaseCard(Model.SummonType.Target, Host.Owner);
+                    context.Game.PlayCard(card);
+                });
+            }
+        }
+
+        [BehaviorModel("人偶召唤", typeof(Passive_AliceDollSummon))]
+        public class ModelType : BehaviorModel
+        {
+            public CardModelReference SummonType { get; set; }
+        }
+    }
+}
