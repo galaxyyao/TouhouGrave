0662cb83d5729e0de3d35c61069f7b132bae3988
Hao Yao <galaxy.yao@gmail.com>
2012-07-17 12:45:28 +0800

    Add restriction that hero card could not attack in the 1st round change warrior flandre's attack and defense

--- trunk/TouhouSpring/Game/Behaviors/Hero.cs
+++ trunk/TouhouSpring/Game/Behaviors/Hero.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace TouhouSpring.Behaviors
 {
-    public class Hero : BaseBehavior<Hero.ModelType>
+    public class Hero : BaseBehavior<Hero.ModelType>,
+        ITrigger<Triggers.AttackPhaseStartedContext>
     {
+        public void Trigger(Triggers.AttackPhaseStartedContext context)
+        {
+            if (context.Game.Round == 1)
+            {
+                Host.Behaviors.Get<Warrior>().State = WarriorState.CoolingDown;
+            }
+        }
+
         [BehaviorModel(typeof(Hero), Category = "Core", Description = "The card is served as the main character.")]
         public class ModelType : BehaviorModel
         {
