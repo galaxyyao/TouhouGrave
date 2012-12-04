ca4bb9669fd4e509351729df0e85960bf11ee7ad
zeroyao <zero.xlyao@gmail.com>
2012-12-04 22:02:15 +0800

    Moved ValueModifier related code to Warrior.ValueModifier.cs.

---
+++ trunk/TouhouSpring/Game/Behaviors/Warrior.ValueModifier.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public partial class Warrior
+    {
+        public class ValueModifier
+        {
+            public enum Operators
+            {
+                Add,
+                Multiply,
+                DivideRoundUp,
+                DivideRoundDown
+            }
+
+            public Operators Operator { get; private set; }
+            public int Amount { get; private set; }
+
+            public ValueModifier(Operators op, int amount)
+            {
+                switch (op)
+                {
+                    case Operators.Add:
+                        if (amount == 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Amount should not be zero.");
+                        }
+                        break;
+                    case Operators.Multiply:
+                        if (amount <= 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Amount should be greater than zero.");
+                        }
+                        break;
+                    case Operators.DivideRoundUp:
+                    case Operators.DivideRoundDown:
+                        if (amount <= 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Amount should be greater than zero.");
+                        }
+                        break;
+                    default:
+                        throw new ArgumentException("op");
+                }
+
+                Operator = op;
+                Amount = amount;
+            }
+
+            internal int Process(int input)
+            {
+                switch (Operator)
+                {
+                    case Operators.Add:
+                        return Math.Max(input + Amount, 0);
+                    case Operators.Multiply:
+                        return Math.Max(input * Amount, 0);
+                    case Operators.DivideRoundUp:
+                        return Math.Max((int)Math.Ceiling((float)input / Amount), 0);
+                    case Operators.DivideRoundDown:
+                        return Math.Max((int)Math.Floor((float)input / Amount), 0);
+                    default:
+                        throw new ArgumentException("Operator");
+                }
+            }
+        }
+
+        private List<ValueModifier> m_attackModifers = new List<ValueModifier>();
+        private List<ValueModifier> m_defenseModifiers = new List<ValueModifier>();
+    }
+}
--- trunk/TouhouSpring/Game/Behaviors/Warrior.cs
+++ trunk/TouhouSpring/Game/Behaviors/Warrior.cs
@@ -12,74 +12,9 @@ namespace TouhouSpring.Behaviors
         CoolingDown
     }
 
-    public class Warrior : BaseBehavior<Warrior.ModelType>,
+    public partial class Warrior : BaseBehavior<Warrior.ModelType>,
         ITrigger<Triggers.CardLeftBattlefieldContext>
     {
-        public class ValueModifier
-        {
-            public enum Operators
-            {
-                Add,
-                Multiply,
-                DivideRoundUp,
-                DivideRoundDown
-            }
-
-            public Operators Operator { get; private set; }
-            public int Amount { get; private set; }
-
-            public ValueModifier(Operators op, int amount)
-            {
-                switch (op)
-                {
-                    case Operators.Add:
-                        if (amount == 0)
-                        {
-                            throw new ArgumentOutOfRangeException("Amount should not be zero.");
-                        }
-                        break;
-                    case Operators.Multiply:
-                        if (amount <= 0)
-                        {
-                            throw new ArgumentOutOfRangeException("Amount should be greater than zero.");
-                        }
-                        break;
-                    case Operators.DivideRoundUp:
-                    case Operators.DivideRoundDown:
-                        if (amount <= 0)
-                        {
-                            throw new ArgumentOutOfRangeException("Amount should be greater than zero.");
-                        }
-                        break;
-                    default:
-                        throw new ArgumentException("op");
-                }
-
-                Operator = op;
-                Amount = amount;
-            }
-
-            internal int Process(int input)
-            {
-                switch (Operator)
-                {
-                    case Operators.Add:
-                        return Math.Max(input + Amount, 0);
-                    case Operators.Multiply:
-                        return Math.Max(input * Amount, 0);
-                    case Operators.DivideRoundUp:
-                        return Math.Max((int)Math.Ceiling((float)input / Amount), 0);
-                    case Operators.DivideRoundDown:
-                        return Math.Max((int)Math.Floor((float)input / Amount), 0);
-                    default:
-                        throw new ArgumentException("Operator");
-                }
-            }
-        }
-
-        private List<ValueModifier> m_attackModifers = new List<ValueModifier>();
-        private List<ValueModifier> m_defenseModifiers = new List<ValueModifier>();
-
         public WarriorState State
         {
             get; private set;
