d0156e98a890e6d818b05b9e588a2081bbfd48a1
zeroyao <zero.xlyao@gmail.com>
2012-12-15 23:43:28 +0800

    Enhanced visual hint of clickable card.

--- trunk/TouhouSpring/THSXnaWindows/THSXnaWindows/UI/CardControlAddins/Highlight.cs
+++ trunk/TouhouSpring/THSXnaWindows/THSXnaWindows/UI/CardControlAddins/Highlight.cs
@@ -2,33 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using XnaMatrix = Microsoft.Xna.Framework.Matrix;
 
 namespace TouhouSpring.UI.CardControlAddins
 {
 	class Highlight : CardControl.Addin
 	{
 		private Graphics.TexturedQuad m_quadHighlight;
+        private Animation.Track m_animationTrack;
 
 		public Highlight(CardControl control) : base(control)
 		{
 			m_quadHighlight = new Graphics.TexturedQuad(GameApp.Service<Services.ResourceManager>().Acquire<Graphics.VirtualTexture>("Textures/CardHighlight"));
-			m_quadHighlight.BlendState = BlendState.Additive;
+            m_quadHighlight.BlendState = new BlendState { ColorSourceBlend = Blend.SourceAlpha, ColorDestinationBlend = Blend.One };
+            m_quadHighlight.ColorToModulate = Color.Lime;
+
+            m_animationTrack = new Animation.CurveTrack(GameApp.Service<Services.ResourceManager>().Acquire<Curve>("Curve_CardFloat"));
+            m_animationTrack.Elapsed += w =>
+            {
+                m_quadHighlight.ColorToModulate.A = (byte)(((Control.MouseTracked.MouseEntered ? 1.0f : w) / 2 + 0.5f) * 255);
+            };
+            m_animationTrack.Loop = true;
+            m_animationTrack.Play();
 		}
+
+        public override void Update(float deltaTime)
+        {
+            m_animationTrack.Elapse(deltaTime);
+        }
 
 		public override void Dispose()
 		{
 			GameApp.Service<Services.ResourceManager>().Release(m_quadHighlight.Texture);
 		}
 
-		public override void RenderMain(XnaMatrix transform, RenderEventArgs e)
+        public override void RenderPostMain(Matrix transform, RenderEventArgs e)
 		{
             bool highlightable = Control.Brightness == 1f && Control.Saturate == 1f;
 
-            if (Control.MouseTracked.MouseEntered && highlightable)
+            if (highlightable
+                && GameApp.Service<Services.GameUI>().ZoomedInCard != Control)
 			{
-				e.RenderManager.Draw(m_quadHighlight, Control.Region, transform);
+                var xo = (m_quadHighlight.Texture.Width - Control.Region.Width) / 2;
+                var yo = (m_quadHighlight.Texture.Height - Control.Region.Height) / 2;
+                var region = new Rectangle(Control.Region.Left - xo, Control.Region.Top - yo, m_quadHighlight.Texture.Width, m_quadHighlight.Texture.Height);
+                
+				e.RenderManager.Draw(m_quadHighlight, region, transform);
 			}
 		}
 	}
