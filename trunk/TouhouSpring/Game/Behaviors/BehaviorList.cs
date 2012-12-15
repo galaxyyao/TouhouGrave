a4a034fd5dcf7a707beac6fff40d6258d73eae6b
zeroyao <zero.xlyao@gmail.com>
2012-12-15 15:00:53 +0800

    Implemented new BehaviorList which has a more strict public interface.

--- trunk/TouhouSpring/Game/Behaviors/BehaviorList.cs
+++ trunk/TouhouSpring/Game/Behaviors/BehaviorList.cs
@@ -7,167 +7,113 @@ using System.Text;
 
 namespace TouhouSpring.Behaviors
 {
-	/// <summary>
-	/// Collection of behavior objects.
-	/// </summary>
-	public class BehaviorList : IList<IBehavior>
-	{
-		private BaseCard m_host;
+    /// <summary>
+    /// Collection of behavior objects.
+    /// </summary>
+    public class BehaviorList : IIndexable<IBehavior>
+    {
+        private BaseCard m_host;
         private List<IBehavior> m_behaviors = new List<IBehavior>();
 
-		#region IList<T> interface
+        #region IIndexable<T> interface
 
         public IBehavior this[int index]
-		{
-			get { return m_behaviors[index]; }
-			set { throw new NotSupportedException(); }
-		}
+        {
+            get { return m_behaviors[index]; }
+        }
 
-        public int IndexOf(IBehavior item)
-		{
-			return m_behaviors.IndexOf(item);
-		}
+        public int Count
+        {
+            get { return m_behaviors.Count; }
+        }
 
-        public void Insert(int index, IBehavior item)
-		{
-			if (index < 0 || index > m_behaviors.Count)
-			{
-				throw new ArgumentOutOfRangeException("index", "Index is out of range.");
-			}
-			else if (item == null)
-			{
-				throw new ArgumentNullException("item");
-			}
-			else if (item.Host != null)
-			{
-				throw new ArgumentException("Item has already been bound.");
-			}
-			else if (m_behaviors.Contains(item))
-			{
-				throw new ArgumentException("Item is already in the list.");
-			}
+        #endregion
 
-			m_behaviors.Insert(index, item);
-			(item as IInternalBehavior).Bind(m_host);
-		}
-
-		public void RemoveAt(int index)
-		{
-			if (index < 0 || index > m_behaviors.Count)
-			{
-				throw new ArgumentOutOfRangeException("index", "Index is out of range.");
-			}
-
-			Debug.Assert(m_behaviors[index].Host == m_host);
-			(m_behaviors[index] as IInternalBehavior).Unbind();
-			m_behaviors.RemoveAt(index);
-		}
-
-		#endregion
-
-		#region ICollection<T> interface
-
-		public int Count
-		{
-			get { return m_behaviors.Count; }
-		}
-
-		public bool IsReadOnly
-		{
-			get { return false; }
-		}
-
-        public void Add(IBehavior item)
-		{
-			if (item == null)
-			{
-				throw new ArgumentNullException("item");
-			}
-			else if (item.Host != null)
-			{
-				throw new ArgumentException("Item has already been bound.");
-			}
-			else if (m_behaviors.Contains(item))
-			{
-				throw new ArgumentException("Item is already in the list.");
-			}
-
-			m_behaviors.Add(item);
-			(item as IInternalBehavior).Bind(m_host);
-		}
-
-		public void Clear()
-		{
-			m_behaviors.ForEach(bhv => (bhv as IInternalBehavior).Unbind());
-			m_behaviors.Clear();
-		}
-
-        public bool Contains(IBehavior item)
-		{
-			return m_behaviors.Contains(item);
-		}
-
-        public void CopyTo(IBehavior[] array, int arrayIndex)
-		{
-			m_behaviors.CopyTo(array, arrayIndex);
-		}
-
-        public bool Remove(IBehavior item)
-		{
-			if (item == null)
-			{
-				throw new ArgumentNullException("item");
-			}
-
-			int index = m_behaviors.IndexOf(item);
-			if (index != -1)
-			{
-				Debug.Assert(item.Host == m_host);
-				(item as IInternalBehavior).Unbind();
-				m_behaviors.RemoveAt(index);
-				return true;
-			}
-
-			return false;
-		}
-
-		#endregion
-
-		#region IEnumerable<T> interface
+        #region IEnumerable<T> interface
 
         public IEnumerator<IBehavior> GetEnumerator()
-		{
-			return m_behaviors.GetEnumerator();
-		}
+        {
+            return m_behaviors.GetEnumerator();
+        }
 
-		#endregion
+        #endregion
 
-		#region IEnumerable interface
+        #region IEnumerable interface
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return m_behaviors.GetEnumerator();
-		}
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return m_behaviors.GetEnumerator();
+        }
 
-		#endregion
+        #endregion
 
         public T Get<T>() where T : class, IBehavior
-		{
-			return m_behaviors.FirstOrDefault(bhv => bhv is T) as T;
-		}
+        {
+            return m_behaviors.FirstOrDefault(bhv => bhv is T) as T;
+        }
 
         public bool Has<T>() where T : class, IBehavior
-		{
-			return m_behaviors.Any(bhv => bhv is T);
-		}
+        {
+            return m_behaviors.Any(bhv => bhv is T);
+        }
 
-		internal BehaviorList(BaseCard host)
-		{
-			if (host == null)
-			{
-				throw new ArgumentNullException("host");
-			}
-			m_host = host;
-		}
-	}
+        internal BehaviorList(BaseCard host)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException("host");
+            }
+            m_host = host;
+        }
+
+        internal void Add(IBehavior item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            else if (item.Host != null)
+            {
+                throw new ArgumentException("Item has already been bound.");
+            }
+            else if (m_behaviors.Contains(item))
+            {
+                throw new ArgumentException("Item is already in the list.");
+            }
+
+            m_behaviors.Add(item);
+            (item as IInternalBehavior).Bind(m_host);
+        }
+
+        internal bool Remove(IBehavior item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            int index = m_behaviors.IndexOf(item);
+            if (index != -1)
+            {
+                Debug.Assert(item.Host == m_host);
+                (item as IInternalBehavior).Unbind();
+                m_behaviors.RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        internal void RemoveAt(int index)
+        {
+            if (index < 0 || index > m_behaviors.Count)
+            {
+                throw new ArgumentOutOfRangeException("index", "Index is out of range.");
+            }
+
+            Debug.Assert(m_behaviors[index].Host == m_host);
+            (m_behaviors[index] as IInternalBehavior).Unbind();
+            m_behaviors.RemoveAt(index);
+        }
+    }
 }
