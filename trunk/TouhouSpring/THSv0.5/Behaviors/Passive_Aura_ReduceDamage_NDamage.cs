0ea9b2812b006cb78bc0470ddc4fe15f52ee290b
galaxyyao <galaxy.yao@gmail.com>
2013-01-10 00:51:30 +0800

    Add Passive_Aura_ReduceDamage_NDamage bhv

---
+++ trunk/TouhouSpring/THSv0.5/Behaviors/Passive_Aura_ReduceDamage_NDamage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class Passive_Aura_ReduceDamage_NDamage :
+        BaseBehavior<Passive_Aura_ReduceDamage_NDamage.ModelType>,
+        Commands.ICause,
+        IPrologTrigger<Commands.DealDamageToCard>
+    {
+        void IPrologTrigger<Commands.DealDamageToCard>.Run(Commands.DealDamageToCard command)
+        {
+            //TODO: Modify if Cancel feature is available in Setup phase
+            if (Host.Owner.ActivatedAssist != Host)
+                return;
+            if (command.Cause is Warrior)
+            {
+                Game.IssueCommands(new Commands.HealCard(command.Target, Model.DamageReduced, this));
+            }
+        }
+
+        [BehaviorModel(typeof(Passive_Aura_ReduceDamage_NDamage), DefaultName = "减伤光环")]
+        public class ModelType : BehaviorModel
+        {
+            public int DamageReduced
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
