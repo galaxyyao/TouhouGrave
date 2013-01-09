c311775ccd8a1d5fa10163d280af659796af3f6e
galaxyyao <galaxy.yao@gmail.com>
2013-01-09 23:35:46 +0800

    Add Passive_Bleed_ToEnemiesNDamageEachTurn bhv Add Passive_DamageToSelectedCard_NDamageEachTurn.cs bhv

---
+++ trunk/touhouspring/THSv0.5/Behaviors/Passive_Bleed_ToEnemiesNDamageEachTurn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class Passive_Bleed_ToEnemiesNDamageEachTurn
+        : BaseBehavior<Passive_Bleed_ToEnemiesNDamageEachTurn.ModelType>,
+        Commands.ICause,
+        IEpilogTrigger<Commands.StartTurn>
+    {
+        void IEpilogTrigger<Commands.StartTurn>.Run(Commands.StartTurn command)
+        {
+            if (Game.ActingPlayer == Host.Owner)
+            {
+                foreach (var player in Game.ActingPlayerEnemies)
+                {
+                    Game.IssueCommands(new Commands.DealDamageToPlayer(player, Model.DamageToDeal, this));
+                }
+            }
+        }
+
+        [BehaviorModel(typeof(Passive_Bleed_ToEnemiesNDamageEachTurn), DefaultName = "对敌方玩家回合伤害")]
+        public class ModelType : BehaviorModel
+        {
+            public int DamageToDeal
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ trunk/touhouspring/THSv0.5/Behaviors/Passive_DamageToSelectedCard_NDamageEachTurn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class Passive_DamageToSelectedCard_NDamageEachTurn :
+        BaseBehavior<Passive_DamageToSelectedCard_NDamageEachTurn.ModelType>,
+        Commands.ICause,
+        IEpilogTrigger<Commands.DrawCard>
+    {
+        void IEpilogTrigger<Commands.DrawCard>.Run(Commands.DrawCard command)
+        {
+            //TODO: Need to be modified for 3 or more players' game
+            if (Game.ActingPlayer == Host.Owner
+                && Game.ActingPlayerEnemies.First().CardsOnBattlefield.Count>0)
+            {
+                var selectedCard = new Interactions.SelectCards(
+                    Host.Owner,
+                    Game.ActingPlayerEnemies.First().CardsOnBattlefield.Where(c => c.Behaviors.Has<Warrior>()).ToArray().ToIndexable(),
+                    Interactions.SelectCards.SelectMode.Single,
+                    "指定1张对手的卡，造成伤害").Run();
+
+                BaseCard castTarget = selectedCard[0];
+                Game.IssueCommands(new Commands.DealDamageToCard(castTarget, Model.DamageToDeal, this));
+            }
+        }
+
+        [BehaviorModel(typeof(Passive_DamageToSelectedCard_NDamageEachTurn), DefaultName = "对指定卡造成伤害")]
+        public class ModelType : BehaviorModel
+        {
+            public int DamageToDeal
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
