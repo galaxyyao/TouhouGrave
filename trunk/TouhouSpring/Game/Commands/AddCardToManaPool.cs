2696a889fcae93df0bb6137ff0887171c305c8e1
galaxyyao <galaxy.yao@gmail.com>
2013-01-16 02:03:57 +0800

    Add command AddCardToManaPool to game

---
+++ trunk/TouhouSpring/Game/Commands/AddCardToManaPool.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Commands
+{
+    /// <summary>
+    /// Add a instance of cardmodel to manapool
+    /// </summary>
+    public class AddCardToManaPool : BaseCommand
+    {
+        // TODO: change to serializable behavior ID
+        public ICardModel Model
+        {
+            get;
+            private set;
+        }
+
+        public Player Owner
+        {
+            get;
+            private set;
+        }
+
+        public BaseCard CardToAdd
+        {
+            get;
+            private set;
+        }
+
+        public AddCardToManaPool(ICardModel model, Player owner)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+            else if (owner == null)
+            {
+                throw new ArgumentNullException("owner");
+            }
+
+            Model = model;
+            Owner = owner;
+        }
+
+        internal override void ValidateOnIssue()
+        {
+            if (Model == null)
+            {
+                FailValidation("Card model can't be null.");
+            }
+            Validate(Owner);
+        }
+
+        internal override void ValidateOnRun()
+        {
+        }
+
+        internal override void RunMain()
+        {
+            CardToAdd = new BaseCard(Model, Owner);
+            Owner.AddToSacrificeSorted(CardToAdd);
+        }
+    }
+}
