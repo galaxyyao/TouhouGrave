891850f51aac466f037ad8acb102d2924b1a26fe
galaxyyao <galaxy.yao@gmail.com>
2013-01-16 23:50:11 +0800

    Add UniqueWarrior bhv to make sure there's only one of a kind warrior on one player's battlefield

---
+++ trunk/TouhouSpring/Game/Behaviors/UniqueWarrior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class UniqueWarrior :
+        BaseBehavior<UniqueWarrior.ModelType>,
+        Commands.ICause,
+        IPrerequisiteTrigger<Commands.PlayCard>
+    {
+        CommandResult IPrerequisiteTrigger<Commands.PlayCard>.Run(Commands.PlayCard command)
+        {
+            if (command.CardToPlay == Host
+                && Host.Owner.CardsOnBattlefield.Any(card => card.Model == Host.Model))
+                return CommandResult.Cancel();
+
+            return CommandResult.Pass;
+        }
+
+        [BehaviorModel(typeof(UniqueWarrior), DefaultName = "唯一")]
+        public class ModelType : BehaviorModel
+        {
+        }
+    }
+}
