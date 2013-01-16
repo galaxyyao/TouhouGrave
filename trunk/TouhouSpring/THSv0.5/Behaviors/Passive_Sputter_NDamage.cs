fb3923d1e1619d31411203e28d072c699447369c
galaxyyao <galaxy.yao@gmail.com>
2013-01-16 23:37:32 +0800

    Add Passive_Sputter_NDamage bhv to Koakuma

---
+++ trunk/TouhouSpring/THSv0.5/Behaviors/Passive_Sputter_NDamage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class Passive_Sputter_NDamage:
+        BaseBehavior<Passive_Sputter_NDamage.ModelType>,
+        Commands.ICause,
+        IEpilogTrigger<Commands.DealDamageToCard>
+    {
+        void IEpilogTrigger<Commands.DealDamageToCard>.Run(Commands.DealDamageToCard command)
+        {
+            if (Host.IsOnBattlefield
+                && command.Cause == Host.Behaviors.Get<Warrior>())
+            {
+                Game.IssueCommands(new Commands.DealDamageToPlayer(Game.ActingPlayerEnemies.First(), Model.DamageToDeal, this));
+            }
+        }
+
+        [BehaviorModel(typeof(Passive_Sputter_NDamage), DefaultName = "溅射")]
+        public class ModelType:BehaviorModel
+        {
+            public int DamageToDeal
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
