16d496a6d6caa3b2020f8f9d257009e856fcc632
galaxyyao <galaxy.yao@gmail.com>
2013-01-17 01:58:04 +0800

    Add Assist_MassSputter_NDamage bhv and add it to eirin

---
+++ trunk/TouhouSpring/THSv0.5/Behaviors/Assist_MassSputter_NDamage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors
+{
+    public sealed class Assist_MassSputter_NDamage :
+        BaseBehavior<Assist_MassSputter_NDamage.ModelType>,
+        Commands.ICause,
+        IEpilogTrigger<Commands.DealDamageToCard>,
+        IEpilogTrigger<Commands.DealDamageToPlayer>
+    {
+        void IEpilogTrigger<Commands.DealDamageToCard>.Run(Commands.DealDamageToCard command)
+        {
+            if (Host.Owner.ActivatedAssist == Host
+                && Game.ActingPlayer==Host.Owner
+                && command.Cause is Warrior
+                && ((Warrior)command.Cause).Host.Owner == Host.Owner)
+            {
+                Game.IssueCommands(new Commands.DealDamageToPlayer(Game.ActingPlayerEnemies.First(), Model.DamageToDeal, this));
+            }
+        }
+
+        void IEpilogTrigger<Commands.DealDamageToPlayer>.Run(Commands.DealDamageToPlayer command)
+        {
+            if (Host.Owner.ActivatedAssist == Host
+                && Game.ActingPlayer == Host.Owner
+                && command.Cause is Warrior
+                && ((Warrior)command.Cause).Host.Owner == Host.Owner)
+            {
+                Game.IssueCommands(new Commands.DealDamageToPlayer(Game.ActingPlayerEnemies.First(), Model.DamageToDeal, this));
+            }
+        }
+
+        [BehaviorModel(typeof(Assist_MassSputter_NDamage), DefaultName = "大规模溅射")]
+        public class ModelType : BehaviorModel
+        {
+            public int DamageToDeal
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
