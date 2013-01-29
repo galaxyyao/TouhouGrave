921886ff595021bc78e10479adbaee1632f30028
galaxyyao <galaxy.yao@gmail.com>
2013-01-29 18:07:47 +0800

    Add NetworkRemoteAgent for receiving & react to remote agent

---
+++ trunk/TouhouSpring/THSXnaWindows/THSXnaWindows/Agents/NetworkLocalPlayerAgent.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Network=TouhouSpring.Network;
+
+namespace TouhouSpring.Agents
+{
+    class NetworkLocalPlayerAgent : BaseAgent
+    {
+        Network.Client _client = null;
+
+        public NetworkLocalPlayerAgent(Network.Client client)
+        {
+            _client = client;
+        }
+
+        public override bool OnCardPlayCanceled(Interactions.NotifyCardEvent io)
+        {
+            if (!String.IsNullOrEmpty(io.Message))
+            {
+                GameApp.Service<Services.ModalDialog>().Show(io.Message, () => io.Respond());
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool OnSpellCastCanceled(Interactions.NotifySpellEvent io)
+        {
+            if (!String.IsNullOrEmpty(io.Message))
+            {
+                GameApp.Service<Services.ModalDialog>().Show(io.Message, () => io.Respond());
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool OnTurnStarted(Interactions.NotifyPlayerEvent io)
+        {
+            if (GameApp.Service<Services.GameUI>().UIState is Services.UIStates.PlayerTransition)
+            {
+                (GameApp.Service<Services.GameUI>().UIState as Services.UIStates.PlayerTransition).OnTurnStarted(io);
+                _client.Send("turn started");
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool OnTurnEnded(Interactions.NotifyPlayerEvent io)
+        {
+            GameApp.Service<Services.GameUI>().EnterState(new Services.UIStates.PlayerTransition(), io);
+            return true;
+        }
+
+        public override void OnTacticalPhase(Interactions.TacticalPhase io)
+        {
+            GameApp.Service<Services.GameUI>().EnterState(new Services.UIStates.TacticalPhase(), io);
+        }
+
+        public override void OnSelectCards(Interactions.SelectCards io)
+        {
+            GameApp.Service<Services.GameUI>().EnterState(new Services.UIStates.SelectCards(), io);
+        }
+
+        public override void OnMessageBox(Interactions.MessageBox io)
+        {
+            // translate from Interactions.MessageBox.Button to Services.ModalDialog.Button
+            Services.ModalDialog.Button buttons = 0;
+            if ((io.Buttons & Interactions.MessageBoxButtons.OK) != 0)
+            {
+                buttons |= Services.ModalDialog.Button.OK;
+            }
+            if ((io.Buttons & Interactions.MessageBoxButtons.Cancel) != 0)
+            {
+                buttons |= Services.ModalDialog.Button.Cancel;
+            }
+            if ((io.Buttons & Interactions.MessageBoxButtons.Yes) != 0)
+            {
+                buttons |= Services.ModalDialog.Button.Yes;
+            }
+            if ((io.Buttons & Interactions.MessageBoxButtons.No) != 0)
+            {
+                buttons |= Services.ModalDialog.Button.No;
+            }
+            GameApp.Service<Services.ModalDialog>().Show(io.Text, buttons, btn =>
+            {
+                // translate back...
+                Interactions.MessageBoxButtons ibtn;
+                switch (btn)
+                {
+                    case Services.ModalDialog.Button.OK:
+                        ibtn = Interactions.MessageBoxButtons.OK;
+                        break;
+                    case Services.ModalDialog.Button.Cancel:
+                        ibtn = Interactions.MessageBoxButtons.Cancel;
+                        break;
+                    case Services.ModalDialog.Button.Yes:
+                        ibtn = Interactions.MessageBoxButtons.Yes;
+                        break;
+                    case Services.ModalDialog.Button.No:
+                        ibtn = Interactions.MessageBoxButtons.No;
+                        break;
+                    default:
+                        throw new ArgumentException("btn");
+                }
+                io.Respond(ibtn);
+            });
+        }
+    }
+}
---
+++ trunk/TouhouSpring/THSXnaWindows/THSXnaWindows/Agents/NetworkRemoteAgent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Agents
+{
+    class NetworkRemoteAgent : BaseAgent
+    {
+        Network.Client _client = null;
+
+        public NetworkRemoteAgent(Network.Client client)
+        {
+            _client = client;
+        }
+
+        private struct CardScorePair
+        {
+            public BaseCard Card;
+            public double Score;
+        }
+
+        public override bool OnTurnStarted(Interactions.NotifyPlayerEvent io)
+        {
+            _client.Send("remote turn started");
+
+            return false;
+        }
+
+        public override void OnTacticalPhase(Interactions.TacticalPhase io)
+        {
+            //// sacrifice
+            //var sacrifice = Sacrifice_MakeChoice2(io);
+            //if (sacrifice != null)
+            //{
+            //    io.RespondSacrifice(sacrifice);
+            //    return;
+            //}
+
+            //var playcard = PlayOrActivateCard_MakeChoice(io);
+            //if (io.PlayCardCandidates.Contains(playcard))
+            //{
+            //    // play
+            //    io.RespondPlay(playcard);
+            //    return;
+            //}
+            //else if (io.ActivateAssistCandidates.Contains(playcard))
+            //{
+            //    // activate
+            //    io.RespondActivate(playcard);
+            //    return;
+            //}
+
+            io.RespondPass();
+        }
+
+        public override void OnSelectCards(Interactions.SelectCards io)
+        {
+            io.Respond(io.SelectFromSet.Count != 0
+                       ? new BaseCard[1] { io.SelectFromSet[0] }.ToIndexable()
+                       : Indexable.Empty<BaseCard>());
+        }
+
+        public override void OnMessageBox(Interactions.MessageBox io)
+        {
+            throw new NotImplementedException();
+            //base.OnMessageBox(io);
+        }
+    }
+}
