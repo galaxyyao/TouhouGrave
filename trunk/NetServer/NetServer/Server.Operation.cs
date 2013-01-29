da56c12e45a999e0d274c043a5752c036091b8af
galaxyyao <galaxy.yao@gmail.com>
2013-01-29 21:59:27 +0800

    Add Room related code

---
+++ trunk/NetServer/NetServer/Server.Operation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+using System.Threading;
+
+namespace TouhouSpring.NetServerCore
+{
+    public partial class Server
+    {
+        public void Start()
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("ths");
+            config.Port = _port;
+            config.MaximumConnections = s_maxConnNum;
+
+            _server = new NetServer(config);
+            _server.Start();
+        }
+
+        public void Listen()
+        {
+            while (true)
+            {
+
+                _im = _server.ReadMessage();
+                if (_im == null)
+                    continue;
+                switch (_im.MessageType)
+                {
+                    case NetIncomingMessageType.DebugMessage:
+                    case NetIncomingMessageType.ErrorMessage:
+                    case NetIncomingMessageType.WarningMessage:
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                        string text = _im.ReadString();
+                        Console.WriteLine(text);
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        {
+                            NetConnectionStatus status = (NetConnectionStatus)_im.ReadByte();
+                            string reason = _im.ReadString();
+                            Console.WriteLine(string.Format("{0} - {1}:{2}", NetUtility.ToHexString(_im.SenderConnection.RemoteUniqueIdentifier), status, reason));
+
+                            if (status == NetConnectionStatus.Connected)
+                            {
+                                NetOutgoingMessage om = _server.CreateMessage();
+                                om.Write(string.Format(" enterroom"));
+                                _server.SendMessage(om, _im.SenderConnection, NetDeliveryMethod.ReliableOrdered, 0);
+                            }
+                            else if (status == NetConnectionStatus.Disconnected)
+                            {
+                                //TODO:
+                            }
+                        }
+                        break;
+                    case NetIncomingMessageType.Data:
+                        {
+                            string data = _im.ReadString();
+                            Console.WriteLine(data);
+                            //TODO: Do something to Data
+                            NetOutgoingMessage om = _server.CreateMessage();
+                            om.Write(NetUtility.ToHexString(_im.SenderConnection.RemoteUniqueIdentifier) + " said: " + "aaa");
+                            _server.SendMessage(om, _im.SenderConnection, NetDeliveryMethod.ReliableOrdered, 0);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine(string.Format("Unhandled type: {0} {1} bytes {2}|{3}"
+                            , _im.MessageType
+                            , _im.LengthBytes
+                            , _im.DeliveryMethod
+                            , _im.SequenceChannel
+                            ));
+                        break;
+                }
+            }
+        }
+
+        public void Shutdown()
+        {
+            _server.Shutdown("bye");
+        }
+    }
+}
---
+++ trunk/NetServer/NetServer/Server.Room.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.NetServerCore
+{
+    public partial class Server
+    {
+        private Dictionary<int, Room> _roomList = new Dictionary<int, Room>();
+
+        private int _idleRoomId = -1;
+
+        public int UserEnter(long uid)
+        {
+            if (_idleRoomId == -1)
+            {
+                int createdRoomId = Find1stEmptyRoomNum();
+                Room createdRoom = new Room(createdRoomId, uid);
+                _roomList.Add(createdRoomId, createdRoom);
+                _idleRoomId = createdRoomId;
+                return -1;
+            }
+            _roomList[_idleRoomId].Enter(uid);
+            int enteredRoomId = _idleRoomId;
+            _idleRoomId = -1;
+            return enteredRoomId;
+        }
+
+        private int Find1stEmptyRoomNum()
+        {
+            int i = 1;
+            while (_roomList.ContainsKey(i))
+                i++;
+            return i;
+        }
+
+        enum RoomStatus
+        {
+            Idle, Playing
+        }
+
+        private class Room
+        {
+            public RoomStatus Status
+            {
+                get;
+                private set;
+            }
+
+            public int Id
+            {
+                get;
+                private set;
+            }
+
+            public long Player1Uid
+            {
+                get;
+                private set;
+            }
+
+            public long Player2Uid
+            {
+                get;
+                private set;
+            }
+
+            public Room(int id, long player1Uid)
+            {
+                Status = RoomStatus.Idle;
+                Id = id;
+                Player1Uid = player1Uid;
+                Player2Uid = -1;
+            }
+
+            public void Enter(long player2Uid)
+            {
+                Player2Uid = player2Uid;
+            }
+        }
+    }
+}
