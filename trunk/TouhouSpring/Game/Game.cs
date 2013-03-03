397324ace83b9080ecf27d60f7f189eaf1c87f01
Hao Yao <galaxy.yao@gmail.com>
2013-03-03 14:49:26 +0800

    Split command to separate partial class

--- trunk/TouhouSpring/Game/Game.cs
+++ trunk/TouhouSpring/Game/Game.cs
@@ -36,38 +36,6 @@ namespace TouhouSpring
             get; private set;
         }
 
-        public static class CurrentCommand
-        {
-            public enum InteractionType
-            {
-                TacticalPhase, SelectCards, Others
-            }
-
-            public static InteractionType Type
-            {
-                get;
-                set;
-            }
-
-            public static object Result
-            {
-                get;
-                set;
-            }
-
-            public static int ResultSubjectIndex
-            {
-                get;
-                set;
-            }
-
-            public static int[] ResultParameters
-            {
-                get;
-                set;
-            }
-        }
-
         public BaseController Controller
         {
             get; private set;
