8d04e2d21989f82d24775b86c2c797dad589235c
Hao Yao <galaxy.yao@gmail.com>
2013-03-13 13:14:47 +0800

    Add missing Network Service

---
+++ trunk/TouhouSpring/THSXnaWindows/THSXnaWindows/Services/Network.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THSNetwork = TouhouSpring.Network;
+using System.Threading;
+using System.Configuration;
+
+namespace TouhouSpring.Services
+{
+    public class Network : GameService
+    {
+        public THSNetwork.Client THSClient
+        {
+            get;
+            private set;
+        }
+
+        public override void Startup()
+        {
+            THSClient = new THSNetwork.Client();
+            THSClient.RemoteServerIp = ConfigurationManager.AppSettings["RemoteServerIp"].ToString();
+            THSClient.RemoteServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["RemoteServerPort"].ToString());
+        }
+
+        public override void Shutdown()
+        {
+            THSClient = null;
+        }
+    }
+}
