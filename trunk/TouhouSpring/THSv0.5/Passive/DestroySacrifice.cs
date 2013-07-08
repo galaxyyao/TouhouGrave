fb1931f626882c5df9a784ad21b5106cc9e79284
zeroyao <zero.xlyao@gmail.com>
2013-07-08 22:08:17 +0800

    Added DestroySacrifice.

---
+++ trunk/TouhouSpring/THSv0.5/Passive/DestroySacrifice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouhouSpring.Behaviors.Passive
+{
+    public sealed class DestroySacrifice : BaseBehavior<DestroySacrifice.ModelType>,
+        ILocalEpilogTrigger<Commands.PlayCard>,
+        Commands.ICause
+    {
+        void ILocalEpilogTrigger<Commands.PlayCard>.RunLocalEpilog(Commands.PlayCard command)
+        {
+            if (Game.Players.Count == 2)
+            {
+                var opponent = Game.Players[1 - Host.Owner.Index];
+                if (opponent.CardsSacrificed.Count > 0)
+                {
+                    var randomTarget = opponent.CardsSacrificed[Game.Random.Next(opponent.CardsSacrificed.Count)];
+                    Game.QueueCommands(new Commands.MoveCard(randomTarget, SystemZone.Graveyard, this));
+                }
+            }
+        }
+
+        [BehaviorModel(typeof(DestroySacrifice), Category = "v0.5/Passive", DefaultName = "萧索")]
+        public class ModelType : BehaviorModel
+        { }
+    }
+}
